2718d9179570293590b0c09ba2d2133a0b38ad7e
Karsten Kempe <smartgit@lycero.de>
2022-01-22 22:02:58 +0100

    Spawner Fix

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,20 +6,22 @@ public class Spawner : MonoBehaviour
 {
     [SerializeField] private GameObject prefab;
     private Vector3 spawnlocation;
+    public Transform MoveTarget;
     // Start is called before the first frame update
     private void OnGUI()
     {
         if (GUI.Button(new Rect(10, 10, 100, 20), "Spawn"))
         {
-            spawnlocation = new Vector3(7, -16, 43);
+            spawnlocation = gameObject.transform.position;
 
             for (int x = 0; x < 5; x++)
             {
                 for (int z = 0; z < 5; z++)
                 {
-                    spawnlocation = new Vector3((7 + x), -14.85f, (43 + z));
-                    Debug.Log(spawnlocation);
-                    GameObject Enemy = Instantiate(prefab, spawnlocation, Quaternion.identity);
+                    var spawnlocationCurrent = spawnlocation + new Vector3(x, 0, z);
+                    Debug.Log(spawnlocationCurrent);
+                    GameObject Enemy = Instantiate(prefab, spawnlocationCurrent, Quaternion.identity);
+                    Enemy.GetComponent<Move>().goal = MoveTarget;
 
                 }
             }
